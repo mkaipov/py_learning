331fe24c93423074d85e4d13c75ebf9667dc6853
mkaipov <mkkaipov@gmail.com>
2022-05-11 01:34:14 +0300

    тут другая задача

--- Строки.py
+++ Строки.py
@@ -1,26 +1,19 @@
 n = int(input())
-array_in = []
-search_requests = []
-array_out = []
+list_n = []
+list_negative = []
+list_zeros = []
+list_pozitive = []
 while n > 0:
-    array_in.append(input())
+    x = int(input())
+    if x < 0:
+        list_negative.append(x)
+    elif x == 0:
+        list_zeros.append(x)
+    else:
+        list_pozitive.append(x)
     n -= 1
-k = int(input())
-while k > 0:
-    search_requests.append(input().lower())
-    k -= 1
-counter = 0
-for request in search_requests:
-    for i in range(len(array_in)):
-        if request.lower() in array_in[i].lower():
-            counter += 1
-            if counter == len(search_requests):
-                array_out.append(array_in[i])
-'''
-for item in array_in:
-    for i in range(len(search_requests)):
-        if search_requests[i].lower() in item.lower():
-            if item not in array_out:
-                array_out.append(item)
-'''
-print(*array_out, sep='\n')
+
+list_n.extend(list_negative)
+list_n.extend(list_zeros)
+list_n.extend(list_pozitive)
+print(*list_n, sep='\n')
